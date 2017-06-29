0ddf7060f59185d2f2c31671271cfa207283e21e
amstut_a <arthur.amstutz@epitech.eu>
2017-06-29 08:41:07 +0100

    fixed bugs on prediction and added better compatibility with spark datasets

--- neighbours.scala
+++ neighbours.scala
@@ -1,6 +1,6 @@
 package com.neighbours
 
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{Dataset,Row}
 import org.apache.spark.sql.functions._
 import scala.language.implicitConversions
 import scala.language.reflectiveCalls
@@ -19,37 +19,38 @@ object Utils {
 }
 
 
-class KNNRegressor[T <: Product](private var k: Int) {
+class KNNRegressor(private var k: Int) {
 
-  private var features: Dataset[T] = _
+  private var features: Dataset[Row] = _
   private var labels:   Dataset[Double] = _
 
 
-  def fit(features: Dataset[T], targets: Dataset[Double]): Unit = {
+  def fit(features: Dataset[Row], targets: Dataset[Double]): Unit = {
     this.features = features
     this.labels   = targets
   }
 
 
-  def predict(feature: Dataset[T]): Double = {
+  def predict(feature: Row): Double = {
     val distances = getDistances(feature).zipWithIndex.sortWith { case (a, b) =>
       val (valA, idxA) = a
       val (valB, idxB) = b
       valA < valB
     }
-    distances.take(k).map(_._2).avg
+    val indices = distances.take(k).map(_._2)
+    labels.collect.zipWithIndex.filter { case (v, idx) =>
+      indices.contains(idx)
+    }.map(_._1).toSeq.avg
   }
 
 
-  def getDistances(feature: Dataset[T]): Seq[Double] = {
-    val feat = feature.collect.last.productIterator.toList
-    val pSquared = feature
-      .select(feature.columns.map(col => pow(feature(col), 2)).reduce((c1, c2) => c1 + c2))
-      .head.toSeq.last.asInstanceOf[Double]
+  def getDistances(feature: Row): Seq[Double] = {
+    val feat = feature.toSeq
+    val pSquared = feat.map(_.asInstanceOf[Double]).map(v => v * v).reduce(_+_)
     val qSquared = features
       .select(features.columns.map(col => pow(features(col), 2)).reduce((c1, c2) => c1 + c2) as "sq")
     val prod = features.collect.map { row =>
-      row.productIterator.toList.zipWithIndex.map {
+      row.toSeq.zipWithIndex.map {
         case (e, idx) => e.asInstanceOf[Double] * feat(idx).asInstanceOf[Double]
       }.reduce(_+_) * -2
     }
